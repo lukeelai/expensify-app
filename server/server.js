1a81a2c544cc12f490afb8f91bcc47a0a7952bfc
Luke Lai <LukeL@meyer.com>
2019-07-31 08:26:19 -0700

    listen

--- server/server.js
+++ server/server.js
@@ -4,3 +4,7 @@ const app = express();
 const publicPath = path.join(__dirname, "..", "public");
 
 app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+  console.log("server is up");
+});
