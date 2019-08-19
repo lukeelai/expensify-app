6a3f4a1ca9a93792d8761a25a808e7f88301da0d
Luke Lai <LukeL@meyer.com>
2019-08-19 08:43:03 -0700

    tests

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,16 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgNMCwaAIvjeaD14og9YYV7heLxawEYeo",
-  authDomain: "expensify-9cd8e.firebaseapp.com",
-  databaseURL: "https://expensify-9cd8e.firebaseio.com",
-  projectId: "expensify-9cd8e",
-  storageBucket: "",
-  messagingSenderId: "1013042114351",
-  appId: "1:1013042114351:web:ce538409980e5220"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
+
+console.log(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 
